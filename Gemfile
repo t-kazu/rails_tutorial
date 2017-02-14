0b554608c2acecdbf2ca7354bb653fff35009b0f
t-kazu <s153057.shimane-u.ac.jp>
2017-02-14 14:47:58 +0900

    gem i awesome_print

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'thor', '0.19.1'
+
+gem 'awesome_print'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214012454) do
+ActiveRecord::Schema.define(version: 20170214053108) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "author"
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
