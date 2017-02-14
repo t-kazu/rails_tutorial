814c21cdfa5df50859f30ed69d22d7f4443f9203
t-kazu <s153057.shimane-u.ac.jp>
2017-02-14 15:15:47 +0900

    画像アップロードをBooksに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214053108) do
+ActiveRecord::Schema.define(version: 20170214055924) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170214053108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "author"
+    t.string   "picture"
   end
 
   create_table "entries", force: :cascade do |t|
